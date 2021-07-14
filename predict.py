3e79ba36ff31299880a43ed8f76b059b6a66b647
Sawsan Alowa <63261168+sawsanowa@users.noreply.github.com>
2021-07-15 00:19:32 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -12,9 +12,9 @@ parser.add_argument('--top_k', help="enter a number",type = int, default = 5)
 parser.add_argument('--category_names', help="enter the json file",default = "label_map.json",type=str)
 #parse arguments
 args = parser.parse_args()
-
+#load the model
 model = tf.keras.models.load_model(args.model, custom_objects={'KerasLayer': hub.KerasLayer})
-
+#image processing
 image = Image.open(args.image_path)
 image = np.asarray(image)
 image = tf.convert_to_tensor(image,tf.float32)
@@ -22,7 +22,7 @@ image = tf.image.resize(image,(224,224))
 image /= 255
 image = image.numpy()
 image = np.expand_dims(image,axis=0)
-
+#prediction
 ps=model.predict(image)
 ps=ps[0].tolist()
 t_values, t_indices= tf.math.top_k(ps,k=args.top_k)
@@ -31,16 +31,9 @@ classes= t_indices.numpy()
 classes+=1
 classes=classes.tolist()
 print(probs, classes)
-
+#get class names
 if args.category_names!=None:
     with open(args.category_names, 'r') as f:
         class_names = json.load(f)
     cl_name = [class_names[str(value)] for value in classes]
     print(cl_name)
-        
-
-
-
-    
- 
-    
